0999a847bf16a03e13605bb29d003c3b83042111
badarithm <karolis.markevicius@protonmail.ch>
2022-02-27 18:45:29 +0100

    Plugged in additional collection macros: withinRadius, sortByRadius

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Dtos\DistanceInterface;
 use App\Services\DistanceFormulas\HaversineFormula;
 use App\Services\DistanceFormulas\SurfaceDistanceFormulaInterface;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +29,18 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->singleton(SurfaceDistanceFormulaInterface::class, function(): SurfaceDistanceFormulaInterface {
             return new HaversineFormula();
+        });
+
+        Collection::macro('withinRadius', function (float|int $limit): Collection {
+            return $this->filter(function (DistanceInterface $entry) use ($limit): bool {
+                return $entry->getDistance() <= $limit;
+            });
+        });
+
+        Collection::macro('sortByRadius', function (): Collection {
+            return $this->sortBy(function (DistanceInterface $entry, $key): bool {
+                return $entry->getDistance();
+            });
         });
     }
 }
