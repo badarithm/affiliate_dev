a090badf2610ce974d185dd529ad49852288b434
badarithm <karolis.markevicius@protonmail.ch>
2022-02-27 22:29:59 +0100

    Validation exceptions should be handled as validation exceptions, the rest have default handler

--- app/Http/Requests/AffiliateFileUploadRequest.php
+++ app/Http/Requests/AffiliateFileUploadRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
 use phpDocumentor\Reflection\Types\Collection;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
